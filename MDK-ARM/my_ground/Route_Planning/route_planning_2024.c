54eaaeca7d9f497a69c4ad94b16dc64e779510da
gwc <3416218577@qq.com>
2026-07-22 20:44:50 +0800

    route_planning--- finish

--- MDK-ARM/my_ground/Route_Planning/route_planning_2024.c
+++ MDK-ARM/my_ground/Route_Planning/route_planning_2024.c
@@ -8,37 +8,6 @@
 //建议航线规划使用世界坐标，而航点显示，简单使用普通映射系即可
 
 
-struct shelf_param_t {
-    int16_t origin_x_s;     //货架X 坐标
-    int16_t y_first_col_s;  // 第一列 Y 坐标
-    uint8_t num_cols_uc;    //列数
-    int16_t col_step_s;     //列间距
-};
-
-
-
-//根据点的坐标和yaw角，判断是哪个货架
-const struct shelf_param_t c_shelf_param_pst[] = {
-    /* A */ {.origin_x_s =   0, .y_first_col_s = 75, .num_cols_uc = 3, .col_step_s = 50},
-    /* B */ {.origin_x_s = 150, .y_first_col_s = 75, .num_cols_uc = 3, .col_step_s = 50},
-    /* C */ {.origin_x_s = 200, .y_first_col_s = 75, .num_cols_uc = 3, .col_step_s = 50},
-    /* D */ {.origin_x_s = 350, .y_first_col_s = 75, .num_cols_uc = 3, .col_step_s = 50},
-};
-
-#define SHELF_NUM sizeof(c_shelf_param_pst)/sizeof(c_shelf_param_pst[0])
-
-#define AISLE_Y_1   -25         //U形转换避障的坐标 偏下面的
-#define AISLE_Y_2   225         //U形转换避障的坐标，偏上的坐标
-
-#define YAW_FONT        0
-#define YAW_REAR        180
-
-
-
-//先确定target_st 所在的货架，再进行规划算法即可
-
-
-
 struct Point_3D_t target_st = {.x_s = 200,.y_s = 75,.z_s = 140,.yaw_s = 0}; 
 struct Point_3D_t return_st = {.x_s = 350,.y_s = 250,.z_s = 140,.yaw_s = 0};  //目标的yaw_s 和 
 
@@ -46,63 +15,88 @@ struct Point_3D_t return_st = {.x_s = 350,.y_s = 250,.z_s = 140,.yaw_s = 0};  //
 
 
 
-static void s_identify_shelf_face(const struct Point_3D_t* target_pst,struct shelf_param_t* matched_face_pst )
-{
 
-    if(target_pst->yaw_s == YAW_FONT)  
-    {
-        if (target_pst->x_s > 150)
-        {
-            *matched_face_pst = c_shelf_param_pst[2]; //C面
-        }
-        else
-        {
-            *matched_face_pst = c_shelf_param_pst[0]; //A面
-        }
-        
-    }
-    else if (target_pst->yaw_s == YAW_REAR)
-    {
-        if (target_pst->x_s > 200)
-        {
-            *matched_face_pst = c_shelf_param_pst[3];//B面
-        }
-        else
-        {
-            *matched_face_pst = c_shelf_param_pst[1]; //D面
-        }
+
+#define AISLE_Y_1   -25         //U形转换避障的坐标 偏下面的
+#define AISLE_Y_2   275         //U形转换避障的坐标，偏上的坐标
+
+#define YAW_FONT        0
+#define YAW_REAR        180
+
+
+// 建议：给货架增加ID字段，避免用内存比较
+enum shelf_id_e { SHELF_A = 0, SHELF_B, SHELF_C, SHELF_D } ;
+
+struct shelf_param_t {
+    enum shelf_id_e id;          // ← 新增标识
+    int16_t origin_x_s;
+    int16_t y_first_col_s;
+    uint8_t num_cols_uc;
+    int16_t col_step_s;
+};
+
+const struct shelf_param_t c_shelf_param_pst[] = {
+    [SHELF_A] = {.id = SHELF_A, .origin_x_s =   0, .y_first_col_s = 75, .num_cols_uc = 3, .col_step_s = 50},
+    [SHELF_B] = {.id = SHELF_B, .origin_x_s = 150, .y_first_col_s = 75, .num_cols_uc = 3, .col_step_s = 50},
+    [SHELF_C] = {.id = SHELF_C, .origin_x_s = 200, .y_first_col_s = 75, .num_cols_uc = 3, .col_step_s = 50},
+    [SHELF_D] = {.id = SHELF_D, .origin_x_s = 350, .y_first_col_s = 75, .num_cols_uc = 3, .col_step_s = 50},
+};
+
+#define SHELF_NUM sizeof(c_shelf_param_pst)/sizeof(c_shelf_param_pst[0])
+
+// 修正：统一函数名，通过ID识别货架
+static void s_identify_shelf_face(const struct Point_3D_t* target_pst, 
+                                   const struct shelf_param_t** matched_ppst)
+{
+    if (target_pst->yaw_s == YAW_FONT) {
+        *matched_ppst = (target_pst->x_s > 100) 
+                        ? &c_shelf_param_pst[SHELF_C] 
+                        : &c_shelf_param_pst[SHELF_A];
+    } else if (target_pst->yaw_s == YAW_REAR) {
+        // 注意：x>200 对应D面(origin=350)，x<=250 对应B面(origin=150)
+        *matched_ppst = (target_pst->x_s > 250) 
+                        ? &c_shelf_param_pst[SHELF_D] 
+                        : &c_shelf_param_pst[SHELF_B];
     }
 }
-
-
 
 void route_generate_patrol(const struct Point_3D_t* target_pst)
 {
+    const struct shelf_param_t* shelf_pst = NULL;
+    s_identify_shelf_face(target_pst, &shelf_pst);  
+    
+    if (shelf_pst == NULL) return;  
 
+    struct Point_3D_t wp = {.yaw_s = target_pst->yaw_s, .z_s = target_pst->z_s};
 
-    struct shelf_param_t shelf_target_st;
-
-    s_identify_shelf_face_b(target_pst,&shelf_target_st);     //获得此时的货架
-
-    struct Point_3D_t target_cur_st = {.yaw_s = target_pst->yaw_s,.z_s = target_pst->z_s};
-
-    if (&shelf_target_st == c_shelf_param_pst)                //如果是A面就直接进行飞行就行
+    if (wp.yaw_s == YAW_REAR)
     {
-        target_cur_st.x_s = target_pst->x_s;
-        target_cur_st.y_s = target_pst->y_s;
-        point_3d_add_b(&target_st);
+        point_3d_add_b(&wp);            //先转到指定yaw角
+    }
+    
+
+    if (shelf_pst->id == SHELF_A) {
+        // A面直达
+        wp.x_s = target_pst->x_s;
+        wp.y_s = target_pst->y_s;
+        point_3d_add_b(&wp);
+    } else {
+        wp.y_s = AISLE_Y_1;
+        point_3d_add_b(&wp);
+
+        wp.x_s = target_pst->x_s;  // 保持当前X进入巷道
+        point_3d_add_b(&wp);
+
+        point_3d_add_b(target_pst);  // 最终目标点
     }
 
-
-    
-        
-
-    
-    
-
-
+    // 返程航点（非D面时才添加）
+    if (shelf_pst->id != SHELF_D) {
+        wp.y_s = AISLE_Y_2;
+        wp.x_s = return_st.x_s;   
+        point_3d_add_b(&wp);
+    }
 }
-
 
 
 
